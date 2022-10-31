18d8e0d4476fde2fcdda103733a10dac66091886
Kirandhanwai <kirandhanwai123@gmail.com>
2022-11-01 01:53:18 +0530

    Added:Ability to linked list by adding 30 & 56 to 70

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+public class LinkedList<T> {
+	Node<T> head;
+
+	Node<T> tail;
+
+	public void add(T data) {
+		Node<T> newNode = new Node<>(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.setNext(head);
+			head = newNode;
+		}
+	}
+
+	public void display() {
+		Node<T> temp = head;
+		if (head == null) {
+			System.out.println("Linked List is empty");
+		}
+		while (temp != null) {
+			System.out.print(temp.getData() + " ");
+			temp = temp.getNext();
+		}
+	}
+}
